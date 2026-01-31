422ef3027a10a3b58735f14af5ca79003da85473
Shubham Pandey <shubpandeyham@gmail.com>
2026-01-31 15:51:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const input = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const printDiv = document.querySelector(".print");
+
+// addBtn.addEventListener("click", () => {
+//   if (input.value.trim() === "") {
+//     alert("Please enter a task!");
+//     return;
+//   }
+
+//   const p = document.createElement("p");
+//   p.innerText = input.value.trim();
+
+//   printDiv.appendChild(p);
+
+//   input.value = "";
+// });
+addBtn.addEventListener("click", () => {
+  if (input.value.trim() === "") {
+    alert("Please enter a task!");
+    return;
+  }
+
+  const todo = document.createElement("div");
+  todo.className = "todo";
+
+  const p = document.createElement("p");
+  p.innerText = input.value;
+
+  const del = document.createElement("span");
+  del.innerText = "delete";
+  del.style.cursor = "pointer";
+
+  del.addEventListener("click", () => {
+    todo.remove();
+  });
+
+  todo.appendChild(p);
+  todo.appendChild(del);
+
+  printDiv.appendChild(todo);
+
+  input.value = "";
+});
